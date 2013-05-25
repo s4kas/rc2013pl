0fc9ff0f4f6104f06bb9ea0a80aecf2683c19ca8
brunofgmarques <brunofgmarques@gmail.com>
2013-05-25 22:55:47 +0000

    [BM] Properties

--- src/common/properties/PropertiesManager.java
+++ src/common/properties/PropertiesManager.java
@@ -1,5 +1,42 @@
-package client;
+package common.properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class PropertiesManager {
 	
+	private static final String CLIENT_PROPS_FILENAME = "client_config.properties";
+	
+	public static Properties loadClientProps() {
+		return loadProps(CLIENT_PROPS_FILENAME);
+	}
+	
+	@SuppressWarnings("resource")
+	private static Properties loadProps(String filename) {
+		Properties props = new Properties();
+	    InputStream is = null;
+	 
+	    // First try loading from the current directory
+	    try {
+	        File f = new File(filename);
+	        is = new FileInputStream( f );
+	    }
+	    catch ( Exception e ) { is = null; }
+	    
+	    try {
+	        if ( is == null ) {
+	            // Try loading from classpath
+	            is = PropertiesManager.class.getResourceAsStream(filename);
+	        }
+	 
+	        // Try loading properties from the file (if found)
+	        props.load( is );
+	        is.close();
+	    }
+	    catch ( Exception e ) { }
+	    
+	    return props;
+	}
 }
